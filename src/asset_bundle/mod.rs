302f24fde89003f1592e73710d68f18785c3bec0
Alvin Null <supdrewin@outlook.com>
2025-06-28 04:17:32 -0400

    Extracting methods

--- src/asset_bundle/mod.rs
+++ src/asset_bundle/mod.rs
@@ -38,6 +38,43 @@ pub struct Header {
     archive_flags: ArchiveFlags,
 }
 
+impl Header {
+    pub fn new(
+        signature: String,
+        version: u32,
+        unity_version: String,
+        unity_revision: String,
+        size: i64,
+        compressed_blocks_info_size: u32,
+        uncompressed_blocks_info_size: u32,
+        archive_flags: ArchiveFlags,
+    ) -> Self {
+        Self {
+            signature,
+            version,
+            unity_version,
+            unity_revision,
+            size,
+            compressed_blocks_info_size,
+            uncompressed_blocks_info_size,
+            archive_flags,
+        }
+    }
+
+    pub fn parse(reader: &mut BinaryReader) -> Self {
+        Self::new(
+            reader.read_cstr(),
+            reader.read_u32(),
+            reader.read_cstr(),
+            reader.read_cstr(),
+            reader.read_i64(),
+            reader.read_u32(),
+            reader.read_u32(),
+            reader.read_u32().into(),
+        )
+    }
+}
+
 #[derive(Debug)]
 pub struct AssetBundle {
     header: Header,
@@ -46,16 +83,7 @@ pub struct AssetBundle {
 impl AssetBundle {
     pub fn new(mut reader: BinaryReader) -> Self {
         Self {
-            header: Header {
-                signature: reader.read_cstr(),
-                version: reader.read_u32(),
-                unity_version: reader.read_cstr(),
-                unity_revision: reader.read_cstr(),
-                size: reader.read_i64(),
-                compressed_blocks_info_size: reader.read_u32(),
-                uncompressed_blocks_info_size: reader.read_u32(),
-                archive_flags: reader.read_u32().into(),
-            },
+            header: Header::parse(&mut reader),
         }
     }
 }
